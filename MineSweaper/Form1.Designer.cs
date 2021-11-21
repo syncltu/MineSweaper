d1cc6636f2ee54c5771fb7992aae69bb795045d5
Martynas Blinstrubis <martynas.blinstrubis@gmail.com>
2021-11-21 14:43:53 +0200

    msbuild

--- MineSweaper/Form1.Designer.cs
+++ MineSweaper/Form1.Designer.cs
@@ -48,7 +48,6 @@
       this.textBox1.TabIndex = 0;
       this.textBox1.Text = "Good day sir, select the size of the map";
       this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
       // 
       // button1
       // 
