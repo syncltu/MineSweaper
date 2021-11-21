09928a7f4cf1d261cf5526a7aaf04f6a78b0d0c3
Martynas Blinstrubis <martynas.blinstrubis@gmail.com>
2021-11-21 17:02:35 +0200

    asdasda

--- build-all.bat
+++ build-all.bat
@@ -1,1 +1,3 @@
-MSBuild.exe /t:rebuild
+        SonarScanner.MSBuild.exe begin /k:"testingas" /o:"diroots" /d:sonar.login="${{ secrets.SONAR_TOKEN }}" /d:sonar.host.url="https://sonarcloud.io"
+        MSBuild.exe /t:rebuild
+        SonarScanner.MSBuild.exe end /d:sonar.login="${{ secrets.SONAR_TOKEN }}"
